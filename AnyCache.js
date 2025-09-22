f74b3f9fe5882ed2b0c6d73391a866ac2a6ed238
GaneshKiranKumar <ganeshkirankumar@gmail.com>
2025-09-22 23:54:05 +0530

    Update AnyCache.js

--- AnyCache.js
+++ AnyCache.js
@@ -1,16 +1,16 @@
-import BaseCache from './BaseCache.js';
+import BaseCache from "./BaseCache.js";
 
 class AnyCache extends BaseCache {
   // no validate method
 }
 
-
+//Test
 const cache = new AnyCache();
 
-cache.set("a", "1",5); 
-cache.set("b", 1); 
-console.log(cache.get("a")); 
+cache.set("a", "1", 5);
+cache.set("b", 1);
+console.log(cache.get("a"));
 setTimeout(() => {
-    console.log(cache.get("a")); // null (after 5s)
+  console.log(cache.get("a")); // null (after 5s)
 }, 6000);
 console.log(cache.get("b"));
