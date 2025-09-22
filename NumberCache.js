d4e9e3514e80a86d955202c9eae35ae188e33add
GaneshKiranKumar <ganeshkirankumar@gmail.com>
2025-09-22 23:54:36 +0530

    Update NumberCache.js

--- NumberCache.js
+++ NumberCache.js
@@ -1,27 +1,23 @@
-import BaseCache from './BaseCache.js';
+import BaseCache from "./BaseCache.js";
 
 class NumberCache extends BaseCache {
-
-    beforeSet(key, value) {
-        if (typeof value !== "number") {
-             console.log("Only numbers allowed!"); // we can use throw new Error
-        }
+  beforeSet(key, value) {
+    if (typeof value !== "number") {
+      console.log("Only numbers allowed!"); // we can use throw new Error
     }
+  }
 
-    afterGet(key, value) {
-        console.log(`Key ${key} accessed with value: ${value}`);
-    }
-
+  afterGet(key, value) {
+    console.log(`Key ${key} accessed with value: ${value}`);
+  }
 }
 export default NumberCache;
 
-
-
-#Test
+// Test
 const cache = new NumberCache();
 cache.set("a", 1, 5); // gives 1
 setTimeout(() => {
-    console.log(cache.get("a")); // null (after 5s)
+  console.log(cache.get("a")); // null (after 5s)
 }, 6000);
-console.log(cache.get("a")); 
+console.log(cache.get("a"));
 cache.set("a", "string", 5); //throws error
